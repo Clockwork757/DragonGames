c98eb34b936318a38dbdc96c26d394ad2ead8490
Danny Horowitz <yynnnnaadd@gmail.com>
2018-11-29 11:21:15 -0500

    setup typescript

---
+++ src/App.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+var app = express();
+
+const root = './static/';
+const dirops = { root: root }
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(express.static(root));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', dirops);
+});
+
+var port = 8080;
+
+app.listen(port);
+console.log(`Listening on port ${port}...`);
